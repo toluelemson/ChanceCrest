d5daa8ba27ce1ec2c2dd76c348e3c25219c52fd2
toluwani <toluwani@ut.ee>
2023-08-17 11:38:58 +0300

    Initialize Project
    - Generate Springboot scaffolding
    - Add .gitignore

---
+++ src/main/java/com/bombaylive/chancecrest/ChanceCrestApplication.java
@@ -0,0 +1,13 @@
+package com.bombaylive.chancecrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChanceCrestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChanceCrestApplication.class, args);
+    }
+
+}
