a6af260566480e0d55f40de08b121d9922407678
toluwani <toluwani@ut.ee>
2023-08-19 19:11:12 +0300

    Disable default Spring Boot security auto-configuration.

--- src/main/java/com/bombaylive/chancecrest/ChanceCrestApplication.java
+++ src/main/java/com/bombaylive/chancecrest/ChanceCrestApplication.java
@@ -2,11 +2,12 @@ package com.bombaylive.chancecrest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 /**
  * The main entry point for the ChanceCrest Spring Boot application.
  */
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class ChanceCrestApplication {
 
     /**
