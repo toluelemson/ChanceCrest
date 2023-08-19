0227ef74a6537995ca116bf70e1ca07d9ac7fd88
toluwani <toluwani@ut.ee>
2023-08-19 19:13:15 +0300

    Refactor: Enhanced unit test for makeBet method to validate expected win amount nonNullResponse

--- src/test/java/com/bombaylive/chancecrest/GameControllerTest.java
+++ src/test/java/com/bombaylive/chancecrest/GameControllerTest.java
@@ -1,42 +1,49 @@
 package com.bombaylive.chancecrest;
+
 import com.bombaylive.chancecrest.game.controller.GameController;
 import com.bombaylive.chancecrest.game.dto.BetRequest;
 import com.bombaylive.chancecrest.game.dto.BetResponse;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import org.springframework.http.ResponseEntity;
+
+import java.util.Objects;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 public class GameControllerTest {
 
+    private static final double TEST_BET_AMOUNT = 40.5;
+    private static final int TEST_NUMBER = 50;
+    private static final double EXPECTED_WIN_AMOUNT = 80.19;
+
     @Autowired
     private GameController gameController;
 
-    /**
-     * Tests the makeBet method in GameController for expected win amount.
-     */
-
     @Test
-    public void whenMakingABet_thenResponseShouldNotBeNull() {
-        BetRequest request = new BetRequest();
-        request.setBetAmount(40.5);
-        request.setNumber(50);
+    public void testMakeBet_returnsNonNullResponse() {
+        BetRequest request = createTestBetRequest();
 
-        BetResponse response = gameController.makeBet(request);
+        ResponseEntity<BetResponse> response = gameController.makeBet(request);
 
-        assertNotNull(response, "Expected a valid response from makeBet but got null");
+        assertTrue(response.getStatusCode().is2xxSuccessful(), "Expected a successful response status code from makeBet.");
     }
 
     @Test
-    public void givenBetRequest_whenMakingABet_thenWinAmountShouldMatchExpectation() {
+    public void testMakeBet_returnsExpectedWinAmount() {
+        BetRequest request = createTestBetRequest();
+
+        ResponseEntity<BetResponse> response = gameController.makeBet(request);
+
+        assertEquals(EXPECTED_WIN_AMOUNT, Objects.requireNonNull(response.getBody()).getWinAmount(), "Expected win amount of " + EXPECTED_WIN_AMOUNT + ", but received a different amount");
+    }
+
+    private BetRequest createTestBetRequest() {
         BetRequest request = new BetRequest();
-        request.setBetAmount(40.5);
-        request.setNumber(50);
-
-        BetResponse response = gameController.makeBet(request);
-
-        assertEquals(80.19, response.getWinAmount(), "Expected win amount of 80.19, but received a different amount");
+        request.setBetAmount(TEST_BET_AMOUNT);
+        request.setNumber(TEST_NUMBER);
+        return request;
     }
 }
